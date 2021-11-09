99e68dd59af057c3e22ff0c86b702404b28d1dfb
zuluft <giodzotsenidze@gmail.com>
2021-11-10 02:08:08 +0400

    Initial commit

---
+++ app/src/main/java/com/zuluft/canvasanimsample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.zuluft.canvasanimsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        @Suppress("DEPRECATION")
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        setContentView(R.layout.activity_main)
+    }
+}
